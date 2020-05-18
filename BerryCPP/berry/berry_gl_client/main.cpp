1c2c3937361c013ac40738a04b3882adaf1b8a92
ayy <502765299@qq.com>
2020-05-18 14:12:58 +0800

    +测试 固定 管线  绘制 三角形 的代码

--- BerryCPP/berry/berry_gl_client/main.cpp
+++ BerryCPP/berry/berry_gl_client/main.cpp
@@ -38,6 +38,13 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glBegin(GL_TRIANGLES);
+		glColor3d(1, 0, 1);
+		glVertex2f(-0.5, -0.5);
+		glVertex2f(0, 0.5);
+		glVertex2f(0.5, -0.5);
+		glEnd();
+
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
